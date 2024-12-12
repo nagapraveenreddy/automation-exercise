9b1f4519a91f78be1146d726b4a81ef9dfe99252
nagapraveenreddy <nagapraveenreddytadi@gmail.com>
2024-12-12 18:44:46 +0530

    first commit

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,12 +1,21 @@
 package testBase;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import pageObjects.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseClass {
     public static WebDriver driver;
@@ -15,17 +24,34 @@ public class BaseClass {
     public static SignupPage signupPage;
     public static AccountCreatedPage accountCreatedPage;
     public static DeleteAccountPage deleteAccountPage;
+    public static Logger logger;
+    public static Properties properties;
+
+    @Parameters({"os","browser"})
     @BeforeClass
-    public void setup(){
-        driver = new ChromeDriver();
+    public void setup(String os, String browser) throws IOException {
+
+        switch (browser.toLowerCase()){
+            case "chrome": driver = new ChromeDriver(); break;
+            case "edge" : driver = new EdgeDriver(); break;
+            case "firefox" : driver = new FirefoxDriver(); break;
+            default: System.out.println("No matching browser"); return;
+        }
+        logger = LogManager.getLogger(this.getClass());
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
         signupPage = new SignupPage(driver);
         accountCreatedPage = new AccountCreatedPage(driver);
         deleteAccountPage = new DeleteAccountPage(driver);
+
+        //Loading config.properties file
+        FileReader file = new FileReader("./src//test//resources//config.properties");
+        properties = new Properties();
+        properties.load(file);
+
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://automationexercise.com/");
+        driver.get(properties.getProperty("appURL"));
         driver.manage().window().maximize();
     }
     @AfterClass
--- src/test/java/testCases/TC001_RegisterUser.java
+++ src/test/java/testCases/TC001_RegisterUser.java
@@ -8,18 +8,32 @@ public class TC001_RegisterUser extends BaseClass {
 
     @Test
     public void Test_RegisterUser(){
+        logger.info("*** Started TC001_RegisterUser ***");
         try{
             String activePage = homePage.getActivePage();
+            logger.info("Navigated to homepage and maximized browser");
+
             Assert.assertEquals(activePage,"Home");
+            logger.info("Homepage validation success");
+
             homePage.clickSignupOrLoginLink();
+            logger.info("Clicked on Signup/Login link");
+            Thread.sleep(3000);
+
             boolean signupOrLoginPageStatus = loginPage.isNewUserSignupTextDisplayed();
             Assert.assertTrue(signupOrLoginPageStatus);
-            loginPage.setSignupName("test");
-            loginPage.setSignupEmail("admin@praveen.com");
+            logger.info("Signup/Login page Validation success");
+
+            loginPage.setSignupName(properties.getProperty("name"));
+            loginPage.setSignupEmail(properties.getProperty("email"));
             loginPage.clickSignupButton();
+            logger.info("Provided Name & Email and clicked on Submit button");
+
             boolean signupPageStatus = signupPage.isEnterAccountInformationTextDisplayed();
             Assert.assertTrue(signupPageStatus);
+            logger.info("Signup page Validation success");
 
+            logger.info("Entering signup details.......");
             signupPage.clickTitle_mr_radio();
             if(signupPage.isNameEmpty()){
                 signupPage.setName("test");
@@ -45,22 +59,30 @@ public class TC001_RegisterUser extends BaseClass {
             signupPage.setZipcode("534124");
             signupPage.setMobileNumber("9999999999");
             signupPage.clickCreateAccount();
+            logger.info("Signup details entered successfully and clicked on create account button");
 
             boolean accountCreatedStatus = accountCreatedPage.isAccountCreatedTextDisplayed();
             Assert.assertTrue(accountCreatedStatus);
             accountCreatedPage.clickContinueButton();
+            logger.info("Account created page Validation success and clicked on continue button");
 
             boolean loginStatus = homePage.isLoggedInAsDisplayed();
             Assert.assertTrue(loginStatus);
+            logger.info("Validated User is login status");
 
             homePage.clickDeleteAccount();
+            logger.info("Deleted account");
 
             boolean deleteStatus = deleteAccountPage.isAccountDeletedTextDisplayed();
             Assert.assertTrue(deleteStatus);
             deleteAccountPage.clickContinueButton();
+            logger.info("Account deleted page Validated and clicked on continue button");
         }
         catch (Exception e) {
             Assert.fail();
+        }
+        finally {
+            logger.info("*** Completed TC001_RegisterUser ***");
         }
 
     }
